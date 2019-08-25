a908ec836b2f888960ff668239a2b5b46fb78a2a
panickat <krizkrag@gmail.com>
2019-08-25 12:03:30 -0500

    error al cargar las variables de entorno

--- alarm.py
+++ alarm.py
@@ -1,15 +1,34 @@
 from os import system
+import os
 import requests
 import json
 
-rq = requests.get('https://api.coindesk.com/v1/bpi/currentprice/mxn.json').json()
+def req_bitcoin():
+    rq = requests.get(
+        'https://api.coindesk.com/v1/bpi/currentprice/mxn.json').json()
+    
+    global usd,mxn,usd_move
+    usd = int(rq["bpi"]["USD"]["rate_float"])
+    mxn = int(rq["bpi"]["MXN"]["rate_float"])
+    usd_move = 350
+
+def price_move():
+    req_bitcoin()
+    last_usd = system("launchctl getenv usd") # error devuelve siempre 0
+    
+    if (last_usd == 0) or (usd > last_usd + usd_move) or (usd < last_usd - usd_move):
+        set_env_usd = "launchctl setenv usd '%s'" % (usd)
+        system(set_env_usd)
+        return True
+    else:
+        return False
+
 
 def speech():
-    usd=str(int(rq["bpi"]["USD"]["rate_float"]))[0:3]+"00"
-    mxn=str(int(rq["bpi"]["MXN"]["rate_float"]))[0:3]+"000"
+    _usd = str(usd)[0:3]+"00"
+    _mxn = str(mxn)[0:3]+"000"
 
-    txt="bitcoin en %s dolares y %s pesos" %(usd,mxn)
-    print(txt)
-    system("say -r 205 "+txt)
+    txt = "bitcoin en %s dolares y %s pesos" % (_usd, _mxn)
+    system("say -r 190 "+txt)
 
-speech()
+if price_move(): speech()
