0a9d365636239e444c9a2601fdfc3ce14c1e0b9b
panickat <krizkrag@gmail.com>
2019-08-25 22:07:21 -0500

    fix get_usd_evn() variable local no definida

---
+++ Readme.md
@@ -0,0 +1,30 @@
+Establecer una variable local en sistema
+https://www.schrodinger.com/kb/1842
+
+OS X 10.10
+To set an environment variable, enter the following command:
+launchctl setenv variable "value"
+To find out if an environment variable is set, use the following command:
+launchctl getenv variable
+To clear an environment variable, use the following command:
+launchctl unsetenv variable
+
+
+correr el script
+pipenv shell
+    Launching subshell in virtual environment…
+ . /Users/panic/.local/share/virtualenvs/py-OPFHogRf/bin/activate
+
+Instalacion de un cronjob
+    https://stackoverflow.com/questions/48990067/how-to-run-a-cron-job-with-pipenv
+
+Obtener un shell con el workflow virtual con 
+pipenv --py
+    /Users/panic/.local/share/virtualenvs/py-OPFHogRf/bin/python
+
+Agregar atrea a cronjob
+    * * * * * cada minuto
+    https://crontab.guru/#01_*_*_*_*
+crontab -e
+
+* * * * * /Users/panic/.local/share/virtualenvs/py-OPFHogRf/bin/python /Users/panic/dev/py/alarm.py >/Users/panic/dev/py/log/stdout.log 2>/Users/panic/dev/py/log/stderr.log
--- alarm.py
+++ alarm.py
@@ -13,9 +13,14 @@ def req_bitcoin_val():
     print("request: "+str(live_usd))
 
 def get_usd_env():
-    completed = subprocess.run(
-        ["launchctl", "getenv", "usd"], stdout=subprocess.PIPE)
-    return int(completed.stdout.decode('utf-8'))
+    completed = subprocess.run(["launchctl", "getenv", "usd"], stdout=subprocess.PIPE)
+    usd_local_env = completed.stdout.decode('utf-8')
+    
+    if usd_local_env:
+        return int(usd_local_env)
+    else:
+        set_usd_env()
+        return live_usd
 
 def set_usd_env():
     subprocess.run(["launchctl", "setenv", "usd", str(live_usd)])
@@ -25,13 +30,9 @@ def price_move():
     last_usd = get_usd_env()
     print("price_move: "+ str(last_usd))
     
-    if last_usd == 0:
-        set_usd_env()
-    elif live_usd >= last_usd + usd_move:
-        set_usd_env()
+    if live_usd >= last_usd + usd_move:
         return {"move": True,"variation": live_usd - last_usd, "trend": "alsa"}
     elif live_usd <= last_usd - usd_move:
-        set_usd_env()
         return {"move": True, "variation": last_usd - live_usd, "trend": "baja"}
     else:
         return {"move": False, "variation": last_usd - live_usd, "trend": "nada"}
@@ -41,7 +42,7 @@ def speech(price_moved):
     _usd = str(live_usd)[0:3]+"00"
     _mxn = str(live_mxn)[0:3]+"000"
 
-    txt = "'El bitcoin esta en %s dolares, en %s pesos, con variación de: %s dolares' a la %s" % (
+    txt = "'El, bitcoin esta! en %s dolares, en %s pesos, con variación de: %s dolares' a la %s" % (
         _usd, _mxn, price_moved["variation"], price_moved["trend"])
     subprocess.run(["say","-r 185",txt])
     
